7486d9f359d1d89a880dc3426e0313abb9f8678e
Anton Suchaneck <a.suchaneck@gmail.com>
2019-02-11 15:36:18 +0100

    .

--- Jupyter-IPython/ssh_tunnel_to_jupyter.sh
+++ Jupyter-IPython/ssh_tunnel_to_jupyter.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-ssh anton@REMOTE -tt -L 12345:localhost:12345 -i DESTHOME/.ssh/ed25519 'source activate py3 && jupyter notebook --port 12345 --no-browser'
+USERNAME=asuchaneck
+HOST=11.217.46.194
+JUPYTER_PORT=12345
+CONDA_ENV_NAME=py
+SSH_KEY_PATH=c/Users/VWT737G/.ssh/ed25519
+
+# Your remote ~/.bashrc needs to include the directory of `activate` (from anaconda) unless you specify the full path below
+# e.g. export PATH=$PATH:..../Anaconda/bin
+# Note that standard .bashrc may exit/return at the top for non-interactive logins -> put your export before that
+
+ssh $USERNAME@$HOST -tt -L $JUPYTER_PORT:127.0.0.1:$JUPYTER_PORT -i /drives/$SSH_KEY_PATH "source activate $CONDA_ENV_NAME; jupyter notebook --port $JUPYTER_PORT --no-browser"
+
 # without -tt it will not kill jupyter on disconnect
