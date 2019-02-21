4fb215f12906c4a49e53f192ffe362e29b1e510d
Ant <the.gerenuk@googlemail.com>
2019-02-21 18:27:25 +0100

    .

--- Jupyter-IPython/ssh_tunnel_to_jupyter.sh
+++ Jupyter-IPython/ssh_tunnel_to_jupyter.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-ssh anton@REMOTE -tt -L 12345:localhost:12345 -i DESTHOME/.ssh/ed25519 'source activate py3 && jupyter notebook --port 12345 --no-browser'
-# without -tt it will not kill jupyter on disconnect
+USERNAME=
+HOST=
+JUPYTER_PORT=12345
+SSH_KEY_PATH=              # for Mobaxterm: /drives/c/...
+INIT_COMMANDS="source activate ...; cd ..."
+
+ssh $USERNAME@$HOST -tx -L $JUPYTER_PORT:127.0.0.1:$JUPYTER_PORT -i $SSH_KEY_PATH screen -R -S jupyter "bash -c '$INIT_COMMANDS; jupyter notebook --port $JUPYTER_PORT --no-browser'"
+
+# -t: terminal; -x: disable X11
+# -R will attach to a unique (detached) screen session -  or use the other arguments to create a new one
+# -S named screen session
+# check screen sessions with "screen -list"
+# you can only attach to detached screen sessions
