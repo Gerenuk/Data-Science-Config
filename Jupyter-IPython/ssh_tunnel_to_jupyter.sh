e8b76e48735ae5083cb6fd5cc7e256bcb80448a8
Anton Suchaneck <a.suchaneck@gmail.com>
2019-04-25 11:37:56 +0200

    .

--- Jupyter-IPython/ssh_tunnel_to_jupyter.sh
+++ Jupyter-IPython/ssh_tunnel_to_jupyter.sh
@@ -5,10 +5,10 @@ JUPYTER_PORT=12345
 SSH_KEY_PATH=              # for Mobaxterm: /drives/c/...
 INIT_COMMANDS="source activate ...; cd ..."
 
-ssh $USERNAME@$HOST -tx -L $JUPYTER_PORT:127.0.0.1:$JUPYTER_PORT -i $SSH_KEY_PATH screen -R -S jupyter "bash -c '$INIT_COMMANDS; jupyter notebook --port $JUPYTER_PORT --no-browser'"
+ssh $USERNAME@$HOST -tx -L $JUPYTER_PORT:127.0.0.1:$JUPYTER_PORT -i $SSH_KEY_PATH screen -dR -S jupyter "bash -c '$INIT_COMMANDS; jupyter notebook --port $JUPYTER_PORT --no-browser'"
 
 # -t: terminal; -x: disable X11
-# -R will attach to a unique (detached) screen session -  or use the other arguments to create a new one
+# -dR will attach to a unique (detach first if needed) screen session -  or use the other arguments to create a new one
 # -S named screen session
 # check screen sessions with "screen -list"
 # you can only attach to detached screen sessions
